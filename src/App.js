2ed35e83742e79fd7ab2ff2c64b9c5f94b403729
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-08-01 00:52:15 +0530

    fav ico

--- src/App.js
+++ src/App.js
@@ -41,11 +41,12 @@ const App = () => {
         }
       >
         <Routes>
+          <Route path="/" name="Home" element={<DefaultLayout />} />
+          <Route path="*" element={<Navigate to="/login" />} />
           <Route exact path="/login" name="Login Page" element={<Login />} />
           <Route exact path="/register" name="Register Page" element={<Register />} />
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
           <Route exact path="/500" name="Page 500" element={<Page500 />} />
-          <Route path="*" name="Home" element={<DefaultLayout />} />
         </Routes>
       </Suspense>
     </Router>
