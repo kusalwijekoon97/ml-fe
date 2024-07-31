e9b3f1b212c40d1018d862ef76e6a69ea77c9bdb
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-07-31 23:17:39 +0530

    ui changes

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,23 +1,18 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import {
-  CCloseButton,
-  CSidebar,
-  CSidebarBrand,
-  CSidebarFooter,
-  CSidebarHeader,
-  CSidebarToggler,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
+import { CCloseButton, CSidebar, CSidebarBrand, CSidebarFooter, CSidebarHeader, CSidebarToggler } from '@coreui/react';
+import CIcon from '@coreui/icons-react';
 
-import { AppSidebarNav } from './AppSidebarNav'
+import { AppSidebarNav } from './AppSidebarNav';
 
-import { logo } from 'src/assets/brand/logo'
-import { sygnet } from 'src/assets/brand/sygnet'
+import { logo } from 'src/assets/brand/logo';
+import { sygnet } from 'src/assets/brand/sygnet';
+
+import logoFullBGWhite from '../assets/images/logo/logoFullBGWhite.png';
 
 // sidebar nav config
-import navigation from '../_nav'
+import navigation from '../_nav';
 
 const AppSidebar = () => {
   const dispatch = useDispatch()
@@ -33,24 +28,21 @@ const AppSidebar = () => {
       visible={sidebarShow}
       onVisibleChange={(visible) => {
         dispatch({ type: 'set', sidebarShow: visible })
-      }}
-    >
+      }}>
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+        <CIcon customClassName="sidebar-brand-full" src={logoFullBGWhite} height={32} />
+        <CIcon customClassName="sidebar-brand-narrow" src={logoFullBGWhite} height={32} />
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
           dark
-          onClick={() => dispatch({ type: 'set', sidebarShow: false })}
-        />
+          onClick={() => dispatch({ type: 'set', sidebarShow: false })} />
       </CSidebarHeader>
       <AppSidebarNav items={navigation} />
       <CSidebarFooter className="border-top d-none d-lg-flex">
         <CSidebarToggler
-          onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
-        />
+          onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })} />
       </CSidebarFooter>
     </CSidebar>
   )
