a26b1812f6782e7c213c338262772d0d55f08827
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-08-03 12:48:44 +0530

    category form seperated

--- src/views/pages/category/createCategory.js
+++ src/views/pages/category/createCategory.js
@@ -4,26 +4,20 @@ import React, { useState } from 'react';
 import {
   CCard,
   CCardBody,
-  CForm,
-  CFormLabel,
-  CButton,
   CContainer,
   CRow,
   CCol,
-  CSpinner,
-  CFormFeedback,
-  CFormInput,
-  CInputGroup
+  CSpinner
 } from '@coreui/react';
 import axios from 'axios';
 import { AppFooter, AppHeader, AppSidebar } from '../../../components';
 import CardHeaderWithTitleBtn from '../../../components/cards/CardHeaderWithTitleBtn';
 import CIcon from '@coreui/icons-react';
-import { cilDelete, cilList } from '@coreui/icons';
+import { cilList } from '@coreui/icons';
 import { Link, useNavigate } from 'react-router-dom';
 import base_url from "../../../utils/api/base_url";
 import ResponseAlert from '../../../components/notifications/ResponseAlert';
-import Select from 'react-select';
+import CategoryForm from '../../../components/forms/CategoryForm';
 
 const CreateCategory = () => {
   const navigate = useNavigate();
@@ -52,15 +46,11 @@ const CreateCategory = () => {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setForm({ ...form, [name]: value });
-
-    // Clear error when the user starts typing
     setErrors({ ...errors, [name]: '' });
   };
 
   const handleLibraryChange = (selectedOptions) => {
     setForm({ ...form, library: selectedOptions });
-
-    // Clear error when the user starts selecting options
     setErrors({ ...errors, library: '' });
   };
 
@@ -174,109 +164,19 @@ const CreateCategory = () => {
                     linkTo="/categories"
                   />
                   <CCardBody>
-                    <CForm onSubmit={handleSubmit}>
-                      <CRow>
-                        <CCol xs={6}>
-                          <div className="mb-3">
-                            <CFormLabel htmlFor="name">Category Name <span className='text-danger'>*</span></CFormLabel>
-                            <CFormInput
-                              type="text"
-                              id="name"
-                              name="name"
-                              placeholder="Enter category name"
-                              value={form.name}
-                              onChange={handleChange}
-                              invalid={!!errors.name}
-                            />
-                            <CFormFeedback>{errors.name}</CFormFeedback>
-                          </div>
-                          <div className="mb-3">
-                            <CFormLabel htmlFor="library">Library <span className='text-danger'>*</span></CFormLabel>
-                            <Select
-                              id="library"
-                              name="library"
-                              options={libraryOptions}
-                              isMulti
-                              value={form.library}
-                              onChange={handleLibraryChange}
-                              className={errors.library ? 'is-invalid' : ''} />
-                            {errors.library && <CFormFeedback>{errors.library}</CFormFeedback>}
-                          </div>
-                        </CCol>
-                        <CCol xs={6}>
-                          <CRow>
-                            <CCol xs={12}>
-                              <div className="d-flex justify-content-between">
-                                <CFormLabel>Sub Categories</CFormLabel>
-                                <CButton
-                                  type="button"
-                                  size="sm"
-                                  color="info"
-                                  onClick={addSubCategory}
-                                >
-                                  Add Sub Category
-                                </CButton>
-                              </div>
-                            </CCol>
-                          </CRow>
-
-                          <CRow className='mt-1'>
-                            {form.subCategories.map((subCategory, index) => (
-                              <CCol xs={12} key={subCategory.id}>
-                                <CInputGroup className="mt-2">
-                                  <CFormInput
-                                    placeholder="Sub category name"
-                                    value={subCategory.name}
-                                    onChange={(e) => handleSubCategoryChange(index, e)}
-                                    invalid={errors.subCategories && !!errors.subCategories[index]}
-                                  />
-                                  <CButton
-                                    type="button"
-                                    color="danger"
-                                    variant="outline"
-                                    onClick={() => removeSubCategory(subCategory.id)}
-                                  >
-                                    <CIcon icon={cilDelete} />
-                                  </CButton>
-                                </CInputGroup>
-                                {errors.subCategories && errors.subCategories[index] && (
-                                  <CFormFeedback>{errors.subCategories[index]}</CFormFeedback>
-                                )}
-                              </CCol>
-                            ))}
-                          </CRow>
-                        </CCol>
-                      </CRow>
-
-                      <div className="text-end">
-                        <Link to="/dashboard">
-                          <CButton type="button"
-                            size='sm'
-                            color="danger"
-                            className="me-2"
-                            disabled={loading}>Cancel</CButton>
-                        </Link>
-                        <CButton type="button"
-                          size='sm'
-                          color="secondary"
-                          className="me-2"
-                          onClick={handlePrevious}
-                          disabled={loading}>Previous</CButton>
-                        <CButton
-                          type="submit"
-                          size='sm'
-                          color="success"
-                          disabled={loading}>
-                          {loading ? (
-                            <>
-                              <CSpinner as="span" size="sm" aria-hidden="true" /> Submitting...
-                            </>
-                          ) : (
-                            'Submit'
-                          )}
-                        </CButton>
-                      </div>
-                    </CForm>
+                    <CategoryForm
+                      form={form}
+                      errors={errors}
+                      libraryOptions={libraryOptions}
+                      handleChange={handleChange}
+                      handleLibraryChange={handleLibraryChange}
+                      handleSubCategoryChange={handleSubCategoryChange}
+                      addSubCategory={addSubCategory}
+                      removeSubCategory={removeSubCategory}
+                      handleSubmit={handleSubmit}
+                      handlePrevious={handlePrevious}
+                      loading={loading}
+                    />
                   </CCardBody>
                 </CCard>
               </CCol>
